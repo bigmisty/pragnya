ca1acb8d8502bf952de9d9bfb930d3b7dffbcff7
big <bigmisty@gmail.com>
2022-05-13 00:37:07 +0530

    News

--- scripts/250a3fe6.main.js
+++ scripts/250a3fe6.main.js
@@ -1,1 +1,1 @@
-console.log("'Allo 'Allo!"),$(document).ready(function(){$("#owl-demo").owlCarousel({autoPlay:3e3,items:6,itemsDesktop:[1199,3],itemsDesktopSmall:[979,3]})});
+console.log("'Hi 'Hello!"),$(document).ready(function(){$("#owl-demo").owlCarousel({autoPlay:3e3,items:6,itemsDesktop:[1199,3],itemsDesktopSmall:[979,3]})});
